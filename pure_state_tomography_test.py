d1308fea5621f5f84990b27ef2678b17788d57d6
svenjandura <sven.jandura@gmail.com>
2019-02-28 01:32:57 +0100

    Added pure state mle

---
+++ pure_state_tomography_test.py
@@ -0,0 +1,23 @@
+import qiskit 
+from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister, Aer
+from qiskit.quantum_info import state_fidelity
+from qiskit.providers.aer import noise
+import qiskit.ignis.verification.tomography as tomo
+
+from qiskit.ignis.verification.tomography.fitters.pure_state_mle_fit import pure_state_mle_fit
+
+
+q2 = QuantumRegister(2)
+bell = QuantumCircuit(q2)
+bell.h(q2[0])
+bell.cx(q2[0], q2[1])
+
+qst_bell = tomo.state_tomography_circuits(bell, q2)
+job = qiskit.execute(qst_bell, Aer.get_backend('qasm_simulator'), shots=5000)
+
+tomo_counts_bell = tomo.tomography_data(job.result(), qst_bell)
+
+probs_bell, basis_matrix_bell, weights_bell = tomo.fitter_data(tomo_counts_bell)
+psi_bell, diff = pure_state_mle_fit(probs_bell, basis_matrix_bell)
+print("Psi: {}".format(psi_bell))
+print("Diff: {}".format(diff))
